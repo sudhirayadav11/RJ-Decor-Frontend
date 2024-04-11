55168060040ffdd1d1b023db9f567736384c0054
sudhiraYadav <sudhirayadav4@gmail.com>
2024-04-11 12:41:25 +0545

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,50 +1,66 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <>
-<div id="about" className="relative bg-white overflow-hidden mt-4">
-  <div className="max-w-7xl mx-auto">
-    <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-      <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-        <polygon points="50,0 100,0 50,100 0,100" />
-      </svg>
-      <div className="pt-1" />
-      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div className="sm:text-center lg:text-left">
-        <h2 className="my-6 text-2xl tracking-tight font-extrabold text-sec sm:text-3xl md:text-4xl">
-  We Provide Complete Furniture Product
-</h2>
-<p>
-  Welcome to RJ.Decorator, your one-stop destination for exquisite furniture pieces that elevate your living spaces. Whether you're furnishing your home, office, or any other space, we have the perfect selection to meet your needs.
-  From timeless classics to modern marvels, our collection boasts a wide range of designs crafted with precision and passion. Our commitment to quality ensures that each piece not only enhances the aesthetics of your space but also offers unparalleled comfort and functionality.
-  Explore our curated catalog and discover furniture that reflects your style and personality. With RJ.Decorator, turning your vision into reality has never been easier. 
-  Lochan Yadav, a successful businessman who runs a furniture empire, 
-  is widely regarded as an innovator in the industry. His journey began
-  in a little carpenter shop. Instead of using traditional procedures, 
-   
-   Lochan infused a modern and creative touch into the company. 
-   Under his leadership, the small workshop blossomed into a 
-   thriving enterprise known for its exquisite and contemporary designs.
-   Lochan's dedication to quality and workmanship became a trademark of his 
-   company. One of Lochan's notable achievements was introducing sustainable 
-   practices into the furniture manufacturing process. Concerned about environmental
-   impact, he implemented eco-friendly materials and production methods, setting a 
-   trend in the industry.  This not only gained him corporate responsibility admiration,
-   but it also attracted an increasing number of environmentally concerned customers.
-</p>
+      <div className="container mx-auto">
+        <h2 className="text-center my-6 text-2xl tracking-tight font-extrabold text-sec sm:text-3xl md:text-4xl">
+          Nature inspired designs, Hand-crafted with love.
+        </h2>
+        <p className="text-center  px-12">
+          {" "}
+          Welcome to RJ Decor, your one-stop destination for exquisite furniture
+          pieces that elevate your living spaces. Whether you're furnishing your
+          home, office, or any other space, we have the perfect selection to
+          meet your needs. From timeless classics to modern marvels, our
+          collection boasts a wide range of designs crafted with precision and
+          passion. Our commitment to quality ensures that each piece not only
+          enhances the aesthetics of your space but also offers unparalleled
+          comfort and functionality. Explore our curated catalog and discover
+          furniture that reflects your style and personality. With RJ.Decorator,
+          turning your vision into reality has never been easier.{" "}
+        </p>
+        
+           
 
-        </div>
-      </main>
+        
+      </div>
+
+      <div className="flex justify-between items-center px-8 py-12">
+    <div className="w-1/2 pr-8">
+        <p className="text-start my-4 text-3xl font-semibold capitalize">
+            About the founder
+        </p>
+        <p className="text-justify">
+            Lochan Yadav, a successful businessman who runs a furniture
+            empire, is widely regarded as an innovator in the industry.
+            His journey began in a little carpenter shop. Instead of using
+            traditional procedures, Lochan infused a modern and creative
+            touch into the company. Under his leadership, the small
+            workshop blossomed into a thriving enterprise known for its
+            exquisite and contemporary designs. Lochan's dedication to
+            quality and craftsmanship became a trademark of his company. One
+            of Lochan's notable achievements was introducing sustainable
+            practices into the furniture manufacturing process. Concerned
+            about environmental impact, he implemented eco-friendly
+            materials and production methods, setting a trend in the
+            industry. This not only gained him corporate responsibility
+            admiration, but it also attracted an increasing number of
+            environmentally concerned customers.
+        </p>
     </div>
-  </div>
-  <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-    <img className="h-56 w-full object-cover object-top sm:h-72 md:h-96 lg:w-full lg:h-full" src="/images/products/product5.jpg" alt />
-  </div>
+    <div className="w-1/2 pl-4">
+        <img
+            className="w-full"
+            src="../../public/FB_IMG_1712640101191.jpg"
+            alt="Image of Lochan Yadav"
+        />
+    </div>
 </div>
 
+    
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
